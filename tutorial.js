0d205884de9e5e2e6c42fd98bdb295e85fe8651e
Jagruti Pancham <jagruti.pancham@corp.ola.com>
2022-07-11 14:57:34 +0530

    program to print no in range and skip a point in it

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,107 @@
-let a = 10
-let b = 10
-console.log(a===b)
+//  let b=36
+//  let x=0
+//    x=x+b
+//     console.log(x)
+
+ let a = new Array(1,2,3,4,5)
+ //how to print no in  range and skip one point
+  let i=0
+ for(i=1;i<11;i++){
+  if(i==8){
+    continue;
+  }   
+
+//    console.log(i); 
+//  }
+
+//reverse printing
+// for(i=100;i>=10;i--){
+//     if(i%10==0){
+//         console.log(i)
+//     }
+// }
+
+// function a1(){
+//     let a=10
+//     let b=20
+//     let c=0
+//     c=a+b
+//     console.log(c)
+// }
+// a1()
+
+//initializing array
+// let arr= new Array(10,20,30,40,50);
+// let x= 0;
+// for(let i=0;i<arr;i++)
+// {
+//     x+= arr[i];
+//     console.log(x);
+// }
+
+//initialize String type arrays
+// var1 = new Array("jags","arnav","baby hayat","vikky","komal","aloo");
+// console.log(var1)
+
+//user input
+// let a=20;
+// let b=10;
+// console.log("which operation do u want to perform");
+// console.log("1.add");
+// console.log("2.subtract");
+// console.log("3.multiply");
+// console.log("4.divide")
+// let x= prompt("enter a no");
+// switch(x){
+//     case 1: a=a+b;
+//             console.log(a);
+//             break;
+//     case 2:a=a-b;
+//             console.log(a);
+//             break;
+//     case 3:a=a*b;
+//             console.log(a);
+//             break;
+//     case 4:a=a/b;
+//              console.log(a);
+//              break;
+//     default: console.log("invalid choice");
+//              break;
+
+// }
+// console.log(a);
+
+//class program
+// class Rectangle {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }
+//     // Getter
+//     get area() {return this.Area();}
+//     // Method
+//     Area() { return this.height * this.width;}
+//     get perimeter(){
+//         return 2*this.height+2*this.width;
+//     }
+//   }
+//   let rect = new Rectangle(10, 10);
+//   console.log(rect.area); 
+//   console.log(rect.perimeter);
+
+//   class circle{
+//      constructor(r){
+//         let pi =3.14;
+//         this.r= r;  
+//         }
+//      get area(){
+//         return this.area();
+//         }
+//         area(){
+//        return pi*this.r*this.r;
+//         }
+//     let circle1 = new circle(7);
+//     console.log(circle1.area);
+//   }
+
+
